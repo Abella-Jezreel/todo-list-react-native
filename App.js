8ea00487cb17aa6051b5effcc7625b62d2f8965b
Abella <jezreel.abella@accenture.com>
2024-08-12 14:47:12 +0800

    Building to do list using react native

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "./App.style";
+import React from "react";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello daddy love!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.header}>
+            {/* <Text>Header</Text> */}
+            <Image source={require('./assets/logo.png')} style={styles.image}/>
+          </View>
+          <View style={styles.body}>
+            <Text>Body</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ App.style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f9f9f9"
+    },
+    header: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    body : {
+        flex: 5,
+    },
+    footer: {
+        height: 70,
+    },
+    image: {
+        width: 300,
+        height: 100,
+      },
+})
